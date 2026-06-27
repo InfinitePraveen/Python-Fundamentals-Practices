5f2bf54b931f71ea8db67bd5dead795633d603fd
Praveen Kumar <hellopraveensales@gmail.com>
2026-06-27 09:55:31 +0000

    Adding the completed Problem5 file.

--- Level2 @ STRINGS & LISTS/Problem5 @ Shopping List Manager.py
+++ Level2 @ STRINGS & LISTS/Problem5 @ Shopping List Manager.py
@@ -1,15 +1,30 @@
-# Start with shopping_list = ['apples', 'bananas', 'milk']
 shopping_list = ['apples', 'banana', 'milk']
+# Start with shopping_list = ['apples', 'bananas', 'milk']
 
 # 1. Append 'bread' to the list
-shopping_list.append('bread')
+shopping_list.append("bread")
+print("After adding 'bread' to the list", shopping_list)
 
 # 2. Insert 'eggs' at position 1
 shopping_list.insert(1, 'eggs')
+print("After inserting 'eggs' in index 1", shopping_list)
 
 # 3. Remove 'bananas'
 shopping_list.remove('banana')
+print("After removing 'banana' from the list:", shopping_list)
+
 # 4. Sort the list alphabetically
+shopping_list.sort()
+print("After sorting the list", shopping_list)
+
 # 5. Reverse the list
+shopping_list.reverse()
+print("Reversed list", shopping_list)
+
 # 6. Create a new list with the first 3 items
+new_shopping_list = list(shopping_list[:3])
+print("New Shopping list:", new_shopping_list)
+
 # 7. Print the length of the list
+print("The length of old shopping list is", len(shopping_list))
+print("And the length of new one is:", len(new_shopping_list))
