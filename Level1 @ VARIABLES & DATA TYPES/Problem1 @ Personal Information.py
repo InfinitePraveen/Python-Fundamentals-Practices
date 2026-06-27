1c0a8166e83bded014085c8fa90ebb5904c738ff
Praveen Kumar <hellopraveensales@gmail.com>
2026-06-27 08:11:29 +0000

    Adding the first file

---
+++ Level1 @ VARIABLES & DATA TYPES/Problem1 @ Personal Information.py
@@ -0,0 +1,16 @@
+# Create variables for:
+# - Your first name (string)
+# - Your last name (string)
+# - Your age (integer)
+# - Your height in meters (float)
+# - Whether you are a student (boolean)
+first_name = "Praveen"
+last_name = "Kumar"
+age = 23
+height = 1.60
+student = True
+
+# Print: "My name is [first] [last]. I am [age] years old,
+# [height]m tall. Student: [True/False]"
+
+print(f"My name is {first_name} {last_name}. I am {age} years old,\n{height} m tall. Student: {student}")
