ee0235d8c22dd0c4e9943771a306dab6689caaad
Praveen Kumar <hellopraveensales@gmail.com>
2026-06-27 09:31:13 +0000

    Adding Problem 5 but it's incomplete because I'm getting some terminal problem, Adding to the repository just for restart.

---
+++ Level2 @ STRINGS & LISTS/Problem5 @ Shopping List Manager.py
@@ -0,0 +1,15 @@
+# Start with shopping_list = ['apples', 'bananas', 'milk']
+shopping_list = ['apples', 'banana', 'milk']
+
+# 1. Append 'bread' to the list
+shopping_list.append('bread')
+
+# 2. Insert 'eggs' at position 1
+shopping_list.insert(1, 'eggs')
+
+# 3. Remove 'bananas'
+shopping_list.remove('banana')
+# 4. Sort the list alphabetically
+# 5. Reverse the list
+# 6. Create a new list with the first 3 items
+# 7. Print the length of the list
