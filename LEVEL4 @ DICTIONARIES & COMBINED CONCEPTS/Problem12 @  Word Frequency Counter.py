5ab3e28a762f30c67f8adecbe7bf434bac119c91
Praveen Kumar <hellopraveensales@gmail.com>
2026-06-29 16:19:11 +0000

    Adding new unsolved file of Problem: 11 & 12

---
+++ LEVEL4 @ DICTIONARIES & COMBINED CONCEPTS/Problem12 @  Word Frequency Counter.py
@@ -0,0 +1,8 @@
+text = """the quick brown fox jumps over the lazy dog the fox 
+          is quick the dog is lazy"""
+
+# 1. Split the text into words
+# 2. Count how many times each word appears
+# 3. Store in a dictionary: {'word': count}
+# 4. Print the dictionary
+# 5. Find the most common word
